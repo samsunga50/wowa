07d07c6137c7176d10659787ec5c004f6129c88e
samsunga50 <samsunga50.in.2020@gmail.com>
2022-03-12 17:38:31 +0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/wowa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wowa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Flutter Tutorial',
+    home: MyApp(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(208, 6, 31, 120),
+        title: const Center(
+            child: Text(
+          'CEB Bill Calculator',
+          style: TextStyle(color: Color.fromARGB(255, 221, 115, 15)),
+        )),
+      ),
+      body: const MyCustomForm(),
+    );
+  }
+}
+
+class MyCustomForm extends StatelessWidget {
+  const MyCustomForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: const <Widget>[
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter number of kWh used',
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
